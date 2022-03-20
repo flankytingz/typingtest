1738fb1d3bc3fbf5fd8c2fcbcd52b634769f88ed
flankedgonerogue <flankedgonerogue@gmail.com>
2022-03-20 17:51:33 +0400

    Improved the conditional css formatting. Added a counter to calculate WPM

--- scripting/main.js
+++ scripting/main.js
@@ -1,74 +1,111 @@
 let mainDiv = document.getElementById("main-div")
 let testDiv = document.getElementById("test-div")
+let resultDiv = document.getElementById("result-div")
 let startBtn = document.getElementById("start-btn")
+let playAgainBtn = document.getElementById("play-again-btn")
 let p = document.getElementById("sample-text-p")
-let testString = "Hello, this is a typing test to test your typing speed and maybe even errors?"
+let string = "Hello, this is a typing test to test your typing speed and maybe even errors?"
+let sumArray = []
+let timer = 0
 
-startBtn.onclick = function() {
+startBtn.onclick = function () {
     changeAppState(1)
-
 }
 function changeAppState(state) {
+    console.log(`App state is now: ${state}`)
     switch (state) {
         case 0:
+            mainDiv.hidden = false
+            testDiv.hidden = true
+            resultDiv.hidden = true
             break;
         case 1:
             mainDiv.hidden = true
             testDiv.hidden = false
-            p.innerHTML = `<span class="undone">${testString}</span>`
-            window.addEventListener("keyup", function (e) {userInputListener(e)})
+            renderHTML()
+            window.addEventListener("keydown", userInputListener)
+            break;
+        case 2:
+            timer = (Date.now() - timer)/1000
+            window.removeEventListener("keydown", userInputListener)
+            console.log(timer, string.split(" ").length, (string.split(" ").length/timer * 60))
+            playAgainBtn.onclick = function () {
+                changeAppState(0)
+            }
+            sumArray = []
+            timer = 0
+            testDiv.hidden = true
+            resultDiv.hidden = false
+            break;
     }
-
+    return null
 }
 
-let previousKey = ""
-let sumArray = []
-let a = 0
 function userInputListener(event) {
-    console.log(event.key)
+    if (timer === 0) timer = Date.now()
     let key = event.key
-    if (previousKey === "Shift") {
-        key = event.key.toUpperCase()
-    } else if (event.key === "Backspace") {
+    if (key === "Backspace" && sumArray.length !== 0) {
         sumArray.pop()
-        a--
         renderHTML()
         return
     }
-    if (testString.charAt(a) === key) {
-
+    if (key.length !== 1) return;
+    if (string.charAt(sumArray.length) === key) {
+        sumArray.push(true)
+    } else {
+        sumArray.push(false)
     }
     renderHTML()
-    a++
-
-    previousKey = event.key
 }
 
 function renderHTML() {
-    let string = ""
-    let previousCorrect = false
-    let first = true
+    let previousCorrect
+    let i = 0
+    let string0 = ""
 
-    for (const i in sumArray) {
-        if (first) {
-            if(i[1] === true) {
-                if (previousCorrect) {
-                    string += i[0]
-                } else {
-                    string += "</span><span class='correct'>" + i[0]
-                }
-            } else {
-                if (previousCorrect) {
-                    string += i[0]
-                } else {
-                    string += "</span><span class='incorrect'>" + i[0]
-                }
-            }
-        }
-        first = false
+    if (sumArray.length === 0) {
+        p.innerHTML =`<span class='undone'>${string}</span>`
+        return
+    } else if (sumArray.length === string.length) {
+        changeAppState(2)
+        return;
     }
 
-    console.log(string)
+    // opens the span tag for the first letter
+    if(sumArray[i] === true) {
+        string0 = `<span class='correct'>${string.charAt(i)}`
+        previousCorrect = true
+    } else {
+        string0 = `<span class='incorrect'>${string.charAt(i)}`
+        previousCorrect = false
+    }
+    i++
+
+    while (i < sumArray.length) {
+        if(sumArray[i] === true) {
+            // if the previous letter was correct then it doesn't open a new span tag
+            if (previousCorrect) {
+                string0 += string.charAt(i)
+            } else {
+                // the previous letter was incorrect but the current letter isn't, so it needs to close the span tag and open a new one
+                string0 += "</span><span class='correct'>" + string.charAt(i)
+            }
+            previousCorrect = true
+        } else {
+            // if the previous letter was correct but the current letter isn't, so it needs to close the span tag and open a new one
+            if (previousCorrect) {
+                string0 += "</span><span class='incorrect'>" + string.charAt(i)
+            } else {
+                // the previous letter was incorrect, so it doesn't need to add a new span tag.
+                string0 += string.charAt(i)
+            }
+            previousCorrect = false
+        }
+        i++
+    }
+    // adds the rest of the text length
+    string0 += `</span><span class='undone'>${string.slice(i)}</span>`
+    p.innerHTML = string0
 }
 /*
 states
